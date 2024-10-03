58eaf8f623a951c362da91d9c06ad3de21233f65
samrad29 <sam.radtke99@gmail.com>
2024-10-02 22:36:59 -0500

    adding response to create request

--- treadmillForm.php
+++ treadmillForm.php
@@ -25,9 +25,9 @@ if(isset($_SESSION['login_time'])) {
 // Step 1: Exchange authorization code for access token
 // if we need to change scope, then would need to get a new authorization code and new refresh token
 $auth_url = "https://www.strava.com/oauth/token";
-$client_id = [inser_client_id];
-$client_secret = [insert_client_secret];
-$refresh_token = [insert_client_secret] ;// From the redirect after user authorization
+$client_id = '135336';
+$client_secret = '50fd40b61efec068ccfb5a5ab74a827f43fdf12b';
+$refresh_token = '15c127d4c7f38f3a412c4f9700f3cc9b5484165a'; // From the redirect after user authorization
 
 $fields = [
     'client_id' => $client_id,
@@ -115,5 +115,31 @@ curl_close($ch);
             ?>
         </tbody>
     </table>
+
+    <form action="API/stravaSubmit.php" method="POST">
+        <label for="form_date">Date (yyyy-mm-dd):</label>
+        <input type="text" id="form_date" name="form_date" required><br><br>
+
+        <label for="form_distance">Distance (in miles):</label>
+        <input type="float" id="form_distance" name="form_distance" required><br><br>
+
+        <label for="form_time">Time (in minutes):</label>
+        <input type="text" id="form_time" name="form_time" required><br><br>
+
+        <button type="submit">Submit</button>
+    </form>
+
+        <!-- Display Success or Error Message -->
+        <?php if (isset($_GET['status'])): ?>
+        <div id="statusMessage">
+            <?php
+                if ($_GET['status'] == 'success') {
+                    echo "<p style='color: green;'>New activity created successfully!</p>";
+                } elseif ($_GET['status'] == 'error') {
+                    echo "<p style='color: red;'>There was an error creating the activity.</p>";
+                }
+            ?>
+        </div>
+    <?php endif; ?>
 </body>
 </html>
